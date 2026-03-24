bc8577bc96df65c540db55dfda6419dfccc20a82
Yasmin <yasiunte@gmail.com>
2026-03-24 11:44:34 -0300

    feat: declarações e variaveis js

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+// declaraçoes e variaveis 
+// var, let, const 
+
+// var pode redeclarar e acessar fora do escopo
+// let nao pode redeclarar e so acessa dentro do escopo
+
+//var x = 10;
+// console.log(x);
+//var x = 20;
+//console.log(x);
+//
+//if (true) {
+//    var nome = "fiap";
+//}
+//console.log(nome);
+
+//const taxaJuros = 0.85;
+//taxaJuros = 0.5;
+//console.log(taxaJuros);
+
+const exemplo = ["oi"];
+console.log(typeof exemplo);
+
+let exemplo8;
+console.log(exemplo8);
+
+let exemplo9=null;
+console.log(exemplo9);
