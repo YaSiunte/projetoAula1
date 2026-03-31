320f8994b6aa2794a6f1913d639f65acbf10b0d2
Yasmin <yasiunte@gmail.com>
2026-03-31 10:45:21 -0300

    feat: Operadores aritmeticos, logicos e comparação

--- src/js/script.js
+++ src/js/script.js
@@ -18,11 +18,37 @@
 //taxaJuros = 0.5;
 //console.log(taxaJuros);
 
-const exemplo = ["oi"];
-console.log(typeof exemplo);
+//const exemplo = ["oi"];
+//console.log(typeof exemplo);
 
-let exemplo8;
-console.log(exemplo8);
+//let exemplo8;
+//console.log(exemplo8);
 
-let exemplo9=null;
-console.log(exemplo9);
+//let exemplo9=null;
+//console.log(exemplo9);
+
+
+//OPERADORES ARITMETICOS 
+
+const valor1=20
+const valor2=10
+
+console.log(valor1 + valor2);
+console.log(valor1 / valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+
+//OPERADORES LOGICOS 
+
+console.log(valor2 > valor1)
+console.log(valor1 > 10 && valor2 != 20)
+console.log(valor2 < 5 || valor1 > 50)
+
+//OPERADORES DE COMPARAÇAO 
+
+console.log(valor1 < valor2);
+console.log(valor1 >= valor2);
+console.log(valor1 <= valor2);
+console.log(valor1 != valor2);
+console.log(valor1 == valor2);
+console.log(valor1 === valor2);
